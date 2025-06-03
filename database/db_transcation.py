251a21010187e4fb9196b3dd6394dc18d488c719
unknown <tahapinar32@gmail.com>
2025-06-03 13:24:41 +0300

    A

--- database/db_transcation.py
+++ database/db_transcation.py
@@ -1,5 +1,9 @@
 class DBTranscation():
-    def get_entities(self,table :Table, query:str):
+    def get_entities(self,table :Table, query:str) -> list[BaseEntity]:
         pass
-    def add_new_entity(table: Table, query : str):
+    def add_new_entity(table: Table, query : str) -> bool:
+        pass
+    def update_entity(table: Table, data:dict) -> bool:
+        pass
+    def delete_entity(table : Table, entity_id:str) -> bool:
         pass
